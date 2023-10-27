4f9699e9b3a4dffc8bb26dc0d158650c779787d1
MirzaUsman <codemindscape@gmail.com>
2023-10-27 12:24:03 +0500

    first change

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+
+    strings:["Web Apps developer","UI, UX Designer",
+    "YouTuber", "Coder"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    backDelay: 900,
+    loop:true
+})
